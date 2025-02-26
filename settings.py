9cd061898da1f9d5e8f4310c2df9436056804dc0
𝒜ͯ͐𝓁̄͑𝑒͗̀𝓍͐ͨ𝒞ͦ̈́𝑜ͮ̈́𝒹͊͆𝑒͑̚𝓇͆͂𝟤ͬ͆𝟥̎͂ <112844632+AlexCoder23@users.noreply.github.com>
2025-02-26 16:54:04 +0300

    new-v settings.py

--- settings.py
+++ settings.py
@@ -1,40 +1,15 @@
-from threading import Lock
+import asyncio
+import orjson as json
 
-with Lock():
-	import psycopg2, os
+
+async def add(db, key, val):
+    await db.execute('insert into Settings (key, value) values ($1, $2)', key, val)
 	
-	__conn = psycopg2.connect(
-	  dbname='core',
-	  user='alexcoder23',
-	  password='qywter132',
-	  host='localhost'
-	)
+async def get(db, key):
+	res = await db.fetch('select value from Settings where key = $1', key)
+	if len(res) == 1: return json.loads(res[0][0])
+	elif len(res) > 1: return list(map(lambda el: json.loads(el[0]), res))
+	else: return None
 	
-	__token = 'opqwerty'
-	
-	def get_conn(token):
-	  if token == __token: return __conn
-	  else: return 'poshol nafig'
-	
-	def add(key, val, token):
-	  if token == __token:
-	    cursor = __conn.cursor()
-	    cursor.execute('insert into Settings (key, value) values (%s, %s)', (key, val, ))
-	  else: return 'poshol nafig'
-	
-	def get(key, token):
-	  if token == __token:
-	    cursor = __conn.cursor()
-	    cursor.execute('select value from Settings where key = %s', (key, ))
-	    res = cursor.fetchall()
-	    if len(res) == 1: return res[0][0]
-	    elif len(res) > 1: return list(map(lambda el: el[0], res))
-	    else: return None
-	  return 'poshol nafig'
-	
-	def set(key, val, token):
-	  if token == __token:
-	    cursor = __conn.cursor()
-	    cursor.execute('update Settings set value = %s where key = %s', (val, key, ))
-	  return 'poshol nafig'
-	
+async def set(db, key, val):
+	await db.execute('update Settings set value = $1 where key = $2', val, key)
